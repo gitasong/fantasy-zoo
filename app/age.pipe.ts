13d947a07538b548872500c8166746976ca9e5c0
Nicole Freed <nicole.freed@gmail.com>
2017-08-18 14:36:04 -0700

    build age pipe transform method

--- app/age.pipe.ts
+++ app/age.pipe.ts
@@ -8,4 +8,24 @@ import {Animal} from './animal.model';
 
 export class AgePipe implements PipeTransform {
 
+  transform(input: Animal[], desiredAge) {
+    var output: Animal[] = [];
+    if(desiredAge === "young") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].age < 2) {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    } else if (desiredAge === "mature") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].age >= 2) {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    } else {
+      return input;
+    }
+  }
 }
