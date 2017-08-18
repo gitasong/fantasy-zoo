12867faf09c745029e526fc7db84971471c4c13d
Nicole Freed <nicole.freed@gmail.com>
2017-08-18 12:58:02 -0700

    import EditAnimalComponent, add to declarations list

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,13 +4,15 @@ import { AppComponent }   from './app.component';
 import { FormsModule }  from '@angular/forms';
 import { AnimalListComponent } from './animal-list.component';
 import { NewAnimalComponent } from './new-animal.component';
+import { EditAnimalComponent } from './edit-animal.component';
 
 @NgModule({
   imports: [ BrowserModule,
             FormsModule ],
   declarations: [ AppComponent,
                 AnimalListComponent,
-                NewAnimalComponent ],
+                NewAnimalComponent,
+                EditAnimalComponent ],
   bootstrap:    [ AppComponent ]
 })
 
