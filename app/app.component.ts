f5e602a994e04218a8332b7fa47b4bcc2c092f12
Nicole Freed <nicole.freed@gmail.com>
2017-08-18 10:05:43 -0700

    import Animal model; create master animal list

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,11 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
+  masterAnimalList: Animal[] = [
+  new Animal("arctic fox", "Moon", 2, "carnivore", "Northern Trail", 5, "female", "cool shade", "loud noises"),
+  new Animal("ocelot", "Prince", 4, "carnivore", "Tropical Rain Forest", 6, "male", "lying in the sunshine", "toys that are not rope-based"),
+  new Animal("northwest black-tailed deer", "Tinkerbell", 8, "herbivore", "Northern Trail", 2, "female", "delicate roots and leaves", "loud noises"),
+  new Animal("gorilla", "Kokko", 7, "herbivore", "Primate House", 5, "male", "bananas", "gnats"),
+  new Animal("unicorn", "Luna", 217, "lightarian", "Fantasy Realms", 12, "female", "moonlight", "impure maidens")
+  ];
 }
