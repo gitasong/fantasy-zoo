e1451ab3ca5ca25894f78c3826dfd07ecc86aa9a
Nicole Freed <nicole.freed@gmail.com>
2017-08-18 14:22:06 -0700

    create age.pipe.ts with imports, @Pipe decorator with metadata, export class w/transform

---
+++ app/age.pipe.ts
@@ -0,0 +1,11 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {Animal} from './animal.model';
+
+@Pipe({
+  name: "age",
+  pure: false
+})
+
+export class AgePipe implements PipeTransform {
+
+}
