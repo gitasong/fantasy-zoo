9f15b8ad068cf904a6dec1ca445d8e4afd48e35c
Nicole Freed <nicole.freed@gmail.com>
2017-08-18 10:41:23 -0700

    build animal display loop

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -5,6 +5,19 @@ import { Animal } from './animal.model';
   selector: 'animal-list',
   template: `
   <h2>Animals Currently In Zoo</h2>
+  <ul>
+    <li *ngFor="let currentAnimal of childAnimalList">
+      <h3><strong>Name: </strong> {{currentAnimal.name}}</h3>
+      <h4><strong>Species: </strong> <em>{{currentAnimal.species}}</em></h4>
+      <p><strong>Age: </strong> {{currentAnimal.age}}</p>
+      <p><strong>Diet: </strong> {{currentAnimal.diet}}</p>
+      <p><strong>Location: </strong> {{currentAnimal.location}}</p>
+      <p><strong>Number of Caretakers: </strong> {{currentAnimal.caretakers}}</p>
+      <p><strong>Sex: </strong> {{currentAnimal.sex}}</p>
+      <p><strong>Likes: </strong> {{currentAnimal.likes}}</p>
+      <p><strong>Dislikes: </strong> {{currentAnimal.dislikes}}</p>
+    </li>
+  </ul>
   `
 })
 
