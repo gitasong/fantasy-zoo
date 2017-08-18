981eeea601a14f78a64d410a5798888d782f4562
Nicole Freed <nicole.freed@gmail.com>
2017-08-18 10:54:54 -0700

    create New Animal form w/button click binding

--- app/new-animal.component.ts
+++ app/new-animal.component.ts
@@ -4,6 +4,28 @@ import { Animal } from './animal.model';
 @Component({
   selector: 'new-animal',
   template: `
+  <h2>Log New Animal</h2>
+  <div>
+    <label>Name: </label>
+    <input #newName>
+    <label>Species: </label>
+    <input #newSpecies>
+    <label>Age: </label>
+    <input #newAge>
+    <label>Diet: </label>
+    <input #newDiet>
+    <label>Location: </label>
+    <input #newLocation>
+    <label>Number of Caretakers: </label>
+    <input #newCaretakers>
+    <label>Sex: </label>
+    <input #newSex>
+    <label>Likes: </label>
+    <input #newLikes>
+    <label>Dislikes: </label>
+    <input #newDislikes>
+    <button (click)="submitForm(newName.value, newSpecies.value, newAge.value, newDiet.value, newLocation.value, newCaretakers.value, newSex.value, newLikes.value, newDislikes.value); newName.value=''; newSpecies.value=''; newAge.value=''; newDiet.value=''; newLocation.value=''; newCaretakers.value=''; newSex.value=''; newLikes.value=''; newDislikes.value='';">Add Animal</button>
+  </div>
   `
 })
 
