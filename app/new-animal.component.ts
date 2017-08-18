a05fc39d3fee0b439198436be47e248672586b19
Nicole Freed <nicole.freed@gmail.com>
2017-08-18 10:53:20 -0700

    create new-animal.component.ts with imports, @Component decorator with metadata, export class declaraion

---
+++ app/new-animal.component.ts
@@ -0,0 +1,12 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Animal } from './animal.model';
+
+@Component({
+  selector: 'new-animal',
+  template: `
+  `
+})
+
+export class NewAnimalComponent {
+
+}
