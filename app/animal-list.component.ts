2b7731cba752099d7a713fa1fd4a19fa5847ef59
Nicole Freed <nicole.freed@gmail.com>
2017-08-19 10:26:13 -0700

    position loop-details div inside loop-image div

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -18,19 +18,19 @@ import { Animal } from './animal.model';
   <div class="list-loop">
     <div class="loop-item" *ngFor="let currentAnimal of childAnimalList | age:filterByAge">
       <div class="loop-image">
+        <div class="loop-details">
+          <h3><strong>Name: </strong> {{currentAnimal.name}}</h3>
+          <h4><strong>Species: </strong> <em>{{currentAnimal.species}}</em></h4>
+          <p><strong>Age: </strong> {{currentAnimal.age}}</p>
+          <p><strong>Diet: </strong> {{currentAnimal.diet}}</p>
+          <p><strong>Location: </strong> {{currentAnimal.location}}</p>
+          <p><strong>Number of Caretakers: </strong> {{currentAnimal.caretakers}}</p>
+          <p><strong>Sex: </strong> {{currentAnimal.sex}}</p>
+          <p><strong>Likes: </strong> {{currentAnimal.likes}}</p>
+          <p><strong>Dislikes: </strong> {{currentAnimal.dislikes}}</p>
+          <button (click)="editClick(currentAnimal)">Edit!</button>
+        </div>
         <img src="{{currentAnimal.image}}" class="img-responsive loop-image" alt="{{currentAnimal.name}}, {{currentAnimal.species}}"/>
-      </div>
-      <div class="loop-details">
-        <h3><strong>Name: </strong> {{currentAnimal.name}}</h3>
-        <h4><strong>Species: </strong> <em>{{currentAnimal.species}}</em></h4>
-        <p><strong>Age: </strong> {{currentAnimal.age}}</p>
-        <p><strong>Diet: </strong> {{currentAnimal.diet}}</p>
-        <p><strong>Location: </strong> {{currentAnimal.location}}</p>
-        <p><strong>Number of Caretakers: </strong> {{currentAnimal.caretakers}}</p>
-        <p><strong>Sex: </strong> {{currentAnimal.sex}}</p>
-        <p><strong>Likes: </strong> {{currentAnimal.likes}}</p>
-        <p><strong>Dislikes: </strong> {{currentAnimal.dislikes}}</p>
-        <button (click)="editClick(currentAnimal)">Edit!</button>
       </div>
     </div>
   </div>
