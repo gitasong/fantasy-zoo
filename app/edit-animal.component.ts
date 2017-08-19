8643c2b0fbb949904eef4bf15930d4ef95cba066
Nicole Freed <nicole.freed@gmail.com>
2017-08-18 21:17:47 -0700

    arrange edit-animal form into form-groups

--- app/edit-animal.component.ts
+++ app/edit-animal.component.ts
@@ -8,26 +8,46 @@ import { Animal } from './animal.model';
       <div *ngIf="selectedAnimal">
         <hr>
         <h3>Edit Animal: {{selectedAnimal.name}}</h3>
-        <label>Enter New Name:</label>
-        <input [(ngModel)]="selectedAnimal.name">
-        <label>Enter New Species:</label>
-        <input [(ngModel)]="selectedAnimal.species">
-        <label>Enter New Image:</label>
-        <input [(ngModel)]="selectedAnimal.image">
-        <label>Enter New Age:</label>
-        <input [(ngModel)]="selectedAnimal.age">
-        <label>Enter New Diet:</label>
-        <input [(ngModel)]="selectedAnimal.diet">
-        <label>Enter New Location:</label>
-        <input [(ngModel)]="selectedAnimal.location">
-        <label>Enter New Caretakers:</label>
-        <input [(ngModel)]="selectedAnimal.caretakers">
-        <label>Enter New Sex:</label>
-        <input [(ngModel)]="selectedAnimal.sex">
-        <label>Enter New Likes:</label>
-        <input [(ngModel)]="selectedAnimal.likes">
-        <label>Enter New Dislikes:</label>
-        <input [(ngModel)]="selectedAnimal.dislikes">
+        <div class="form-group">
+          <label>Enter New Name:</label>
+          <input [(ngModel)]="selectedAnimal.name">
+        </div>
+        <div class="form-group">
+          <label>Enter New Species:</label>
+          <input [(ngModel)]="selectedAnimal.species">
+        </div>
+        <div class="form-group">
+          <label>Enter New Image:</label>
+          <input [(ngModel)]="selectedAnimal.image">
+        </div>
+        <div class="form-group">
+          <label>Enter New Age:</label>
+          <input [(ngModel)]="selectedAnimal.age">
+        </div>
+        <div class="form-group">
+          <label>Enter New Diet:</label>
+          <input [(ngModel)]="selectedAnimal.diet">
+        </div>
+        <div class="form-group">
+          <label>Enter New Location:</label>
+          <input [(ngModel)]="selectedAnimal.location">
+        </div>
+        <div class="form-group">
+          <label>Enter New Caretakers:</label>
+          <input [(ngModel)]="selectedAnimal.caretakers">
+        </div>
+        <div class="form-group">
+          <label>Enter New Sex:</label>
+          <input [(ngModel)]="selectedAnimal.sex">
+        </div>
+        <div class="form-group">
+          <label>Enter New Likes:</label>
+          <input [(ngModel)]="selectedAnimal.likes">
+        </div>
+        <div class="form-group">
+          <label>Enter New Dislikes:</label>
+          <input [(ngModel)]="selectedAnimal.dislikes">
+        </div>
         <button (click)="doneClick()">Done</button>
       </div>
     </div>
